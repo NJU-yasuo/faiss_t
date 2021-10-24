a902f142bdf6697faed5b52167089594183a9ade
NJU-yasuo <50507540+NJU-yasuo@users.noreply.github.com>
2021-10-24 14:50:57 +0800

    Update build-lib.sh

--- build-lib.sh
+++ build-lib.sh
@@ -2,14 +2,15 @@
 
 # Build libfaiss.so/libfaiss_avx2.so.
 ../cmake/bin/cmake -B _build \
+      -DBUILD_SHARED_LIBS=ON \
       -DBUILD_TESTING=OFF \
       -DFAISS_ENABLE_GPU=OFF \
       -DFAISS_ENABLE_PYTHON=ON \
       -DPython_EXECUTABLE=$PYTHON \
       -DBLA_VENDOR=Intel10_64_dyn \
       -DFAISS_OPT_LEVEL=avx2 \
-      -DMKL_LIBRARIES=/opt/intel/mkl/lib/intel64/  \
+      -DMKL_LIBRARIES=/root/anaconda3/lib \
+      -DCMAKE_INSTALL_LIBDIR=/root/anaconda3/lib \
       -DCMAKE_BUILD_TYPE=Release .
-
 
 make -C _build -j $CPU_COUNT 
